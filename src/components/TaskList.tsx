0cc7869ca4c258cf8d04c27a43a92609a5bf9ae5
Claudio Matheus <claudiomatheus055@gmail.com>
2024-08-12 13:52:52 -0300

    Exibindo lista de tarefas

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
 
-const TaskList = (props: Props) => {
-  return (
-    <div>
-        <p>lista de tarefas</p>
-    </div>
-  )
+//INTERFACES
+import { ITask } from "../interfaces/Task";
+
+//CSS
+import styles from "./TaskList.module.css";
+
+interface Props {
+  taskList: ITask[];
+
+
 }
+
+const TaskList = ({taskList}: Props) => {
+  return (
+    <>
+        {taskList.length > 0 ?(
+          taskList.map((task)=> (
+            <div key={task.id}>
+              <p>{task.title}</p>
+            </div>
+          ))
+        ):(
+          <p>Não há tarefas cadastradas</p>
+        )}
+    </>
+  );
+};
 
 export default TaskList
